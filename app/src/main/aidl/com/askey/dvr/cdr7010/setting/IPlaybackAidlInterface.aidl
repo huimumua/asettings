141bbd201ec432ffa7582ec612622375bc2ea868
Charles Bai(白超) <charles_bai@askey.com>
2018-04-11 15:22:50 +0800

    增加签名文件及编译生成apk的名称

--- app/src/main/aidl/com/askey/dvr/cdr7010/setting/IPlaybackAidlInterface.aidl
+++ app/src/main/aidl/com/askey/dvr/cdr7010/setting/IPlaybackAidlInterface.aidl
@@ -10,4 +10,10 @@ interface IPlaybackAidlInterface {
      */
     void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
             double aDouble, String aString);
+
+
+
+
+
+
 }
--- app/src/main/java/com/askey/dvr/cdr7010/setting/SettingsActivity.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/SettingsActivity.java
@@ -11,5 +11,7 @@ public class SettingsActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
     }
+
 }
