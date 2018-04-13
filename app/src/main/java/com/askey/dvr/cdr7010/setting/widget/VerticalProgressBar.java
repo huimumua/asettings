8cc0eb8f2ed2ccc8c0dbd0f85cff25d67944b687
lisa-peng <Lisa.peng@cdskysoft.com>
2018-04-13 11:31:32 +0800

    PUCDR-452 Base list view progressBar

--- app/src/main/java/com/askey/dvr/cdr7010/setting/widget/VerticalProgressBar.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/widget/VerticalProgressBar.java
@@ -19,7 +19,7 @@ public class VerticalProgressBar extends View {
 	private int height;// 高度值
 
 	public VerticalProgressBar(Context context, AttributeSet attrs,
-                               int defStyleAttr) {
+							   int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		init();
 	}
@@ -43,16 +43,27 @@ public class VerticalProgressBar extends View {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 		width = getMeasuredWidth() - 1;// 宽度值
 		height = getMeasuredHeight() - 1;// 高度值
-		Log.i("VerticalProgressBar","==height=="+height+"=width=="+width);
+		Log.i("VerticalProgressBar", "==height==" + height + "=width==" + width);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		paint.setColor(Color.rgb(55, 200, 255));// 设置画笔颜色
-		canvas.drawRect(0, (float)progress / total * height, width, height, paint);// 画矩形
+		float progressHeight = (float) 6 * height / total;
+		Log.i("test", "==progressHeight=" + progressHeight);
+		if (progressHeight < 8) {
+			progressHeight = 8;
+		}
+		if ((secondProgress - progress) == 6) {
+			int top = progress * height / total;
+			canvas.drawRect(0, top, width, (top + progressHeight), paint);// 画矩形
+			Log.i("test", "=top=" + top + "=(top + progressHeight)==" + (top + progressHeight));
+		} else if (secondProgress - progress < 6) {//不满一页
+			canvas.drawRect(0, (float) progress / total * height, width, (float) secondProgress / total * height, paint);// 画矩形
+			Log.i("test", "=height - progress / total * height=" + ((float) progress / total * height) + "=width==" + width + "=height=" + height);
+		}
 //		canvas.drawRect(0, height - progress / 100f * height, width, height,
 //				paint);// 画矩形
-		Log.i("test","=height - progress / total * height="+((float)progress / total * height)+"=width=="+width+"=height="+height);
 		canvas.drawLine(0, 0, width, 0, paint);// 画顶边
 		canvas.drawLine(0, 0, 0, height, paint);// 画左边
 		canvas.drawLine(width, 0, width, height, paint);// 画右边
@@ -62,16 +73,17 @@ public class VerticalProgressBar extends View {
 //		paint.setTextSize(width / 3);// 设置文字大小
 //		canvas.drawText(progress + "%",
 //				(width - getTextWidth(progress + "%")) / 2, height / 2, paint);// 画文字
-		paint.setColor(Color.WHITE);// 设置画笔颜色
-		canvas.drawRect(1, (float)secondProgress / total * height, width, height, paint);// 画矩形
+//		paint.setColor(Color.WHITE);// 设置画笔颜色
+//		canvas.drawRect(1, (float)secondProgress / total * height, width, height, paint);// 画矩形
 //		canvas.drawRect(1, height - secondProgress / 100f * height, width, height, paint);// 画矩形
 		super.onDraw(canvas);
 	}
 
 	/**
 	 * 拿到文字宽度
+	 *
 	 * @param str 传进来的字符串
-	 * return 宽度
+	 *            return 宽度
 	 */
 	private int getTextWidth(String str) {
 		// 计算文字所在矩形，可以得到宽高
@@ -80,8 +92,10 @@ public class VerticalProgressBar extends View {
 		return rect.width();
 	}
 
-	/** 设置progressbar进度 */
-	public void setProgress(int progress,int secondProgress,int total) {
+	/**
+	 * 设置progressbar进度
+	 */
+	public void setProgress(int progress, int secondProgress, int total) {
 		this.progress = progress;
 		this.secondProgress = secondProgress;
 		this.total = total;
