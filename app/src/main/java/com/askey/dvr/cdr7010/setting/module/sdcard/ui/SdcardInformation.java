1136b5fd1d92c5eae53068fda398225bfaa2e32f
Charles Bai(白超) <charles_bai@askey.com>
2018-04-19 10:33:05 +0800

    完成PUCDR-406 Sdcard信息展示

--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/sdcard/ui/SdcardInformation.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/sdcard/ui/SdcardInformation.java
@@ -2,6 +2,8 @@ package com.askey.dvr.cdr7010.setting.module.sdcard.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
 import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.SdcardInfo;
@@ -22,12 +24,20 @@ import java.util.List;
  */
 public class SdcardInformation extends BaseActivity{
     private static final String TAG = "SdcardInformation";
+    private TextView normal,event,parking,picture;
+    private String backslash= "/";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sdcard_information);
 
+        initview();
+        initData();
+
+    }
+
+    private void initData() {
         List<SdcardInfo> sdcardInfo = FileManager.getInstance().getSdcardInfo();
         if(sdcardInfo!=null && sdcardInfo.size()>0){
             String totalSize = sdcardInfo.get(0).getTotalSize();
@@ -48,7 +58,29 @@ public class SdcardInformation extends BaseActivity{
             Logg.i(TAG,"==getEventSize=="+eventSize);
             Logg.i(TAG,"==getParkingSize=="+parkingSize);
             Logg.i(TAG,"==getPictureSize=="+pictureSize);
+            normal.setText(normalCurrentSize+backslash+normalSize);
+            event.setText(eventCurrentSize+backslash+eventSize);
+            parking.setText(parkingCurrentSize+backslash+parkingSize);
+            picture.setText(pictureCurrentSize+backslash+pictureSize);
+        }else{
+            TextView title = this.findViewById(R.id.sdcard_setting_information);
+            TextView normanTitle = this.findViewById(R.id.sdcard_normal_dir);
+            TextView eventTitle = this.findViewById(R.id.sdcard_event_dir);
+            TextView parkingTitle = this.findViewById(R.id.sdcard_parking_dir);
+            TextView pictureTitle = this.findViewById(R.id.sdcard_picture_dir);
+            title.setText(getResources().getString(R.string.sdcard_not_exist));
+            normanTitle.setVisibility(View.GONE);
+            eventTitle.setVisibility(View.GONE);
+            parkingTitle.setVisibility(View.GONE);
+            pictureTitle.setVisibility(View.GONE);
         }
+    }
+
+    private void initview() {
+        normal = this.findViewById(R.id.sdcard_normal_dir_count);
+        event = this.findViewById(R.id.sdcard_event_dir_count);
+        parking = this.findViewById(R.id.sdcard_parking_dir_count);
+        picture = this.findViewById(R.id.sdcard_picture_dir_count);
     }
 
 
