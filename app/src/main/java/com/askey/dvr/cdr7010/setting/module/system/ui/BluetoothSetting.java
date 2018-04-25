0078f6e8be0077f280eb7a0ce615c028032533d7
Beck Chen(陈立_Askey_CT) <beck_chen@askey.com.tw>
2018-04-25 18:43:34 +0800

    增加自定义布局

--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/system/ui/BluetoothSetting.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/system/ui/BluetoothSetting.java
@@ -2,8 +2,13 @@ package com.askey.dvr.cdr7010.setting.module.system.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.base.BaseActivity;
+import com.askey.dvr.cdr7010.setting.widget.JVCRelativeLayout;
 
 /**
  * 项目名称：settings
@@ -19,5 +24,19 @@ public class BluetoothSetting extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.bluetooth_setting_layout);
+
+        JVCRelativeLayout jvcRelativeLayout = (JVCRelativeLayout) findViewById(R.id.root);
+        jvcRelativeLayout.setContentView(R.layout.activity_base_nemu);
+        View view = jvcRelativeLayout.getMyView();
+
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BluetoothSetting.this, "ok", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
