9f9ebf672f80506c30ecab8c516dbfc1470cce07
Charles Bai(白超) <charles_bai@askey.com>
2018-05-02 09:08:03 +0800

    增加长按事件0.3秒1秒3秒的方法

--- app/src/main/java/com/askey/dvr/cdr7010/setting/base/BaseActivity.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/base/BaseActivity.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 
 import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.application.SettingApplication;
+import com.askey.dvr.cdr7010.setting.util.Logg;
 
 /**
  * 项目名称：settings
@@ -18,17 +20,58 @@ import com.askey.dvr.cdr7010.setting.application.SettingApplication;
  * 修改备注：
  */
 public class BaseActivity extends AppCompatActivity{
-
     private final  String  TAG = "BaseActivity";
     protected static Context mContext;
-    protected static Context appContext = SettingApplication.getInstance();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base_nemu);
 
         mContext = this;
+    }
+
+
+    private int keydowmRepeatCount =0;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        keydowmRepeatCount = event.getRepeatCount();
+        switch (event.getRepeatCount()){
+            case 10:
+                onKeyHoldHalfASecond(keyCode);
+                break;
+            case 20:
+                onKeyHoldOneSecond(keyCode);
+                break;
+            case 60:
+                onKeyHoldThreeSecond(keyCode);
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if(keydowmRepeatCount < 10){
+            onKeyShortPressed(keyCode);
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+
+    public void onKeyShortPressed(int keyCode) {
+
+    }
+
+    public  void onKeyHoldHalfASecond(int keyCode){
+
+    }
+
+    public  void onKeyHoldOneSecond(int keyCode){
+
+    }
+
+    public  void onKeyHoldThreeSecond(int keyCode){
+
     }
 
 
