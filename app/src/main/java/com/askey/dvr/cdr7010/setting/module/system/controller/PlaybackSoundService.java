41d37e2d898f49ef47989e98de919abfa6320f8c
lisa_peng <lisa_peng@askey.com.tw>
2018-05-08 16:01:38 +0800

    修改更换系统后playbacksound调节出错的问题

--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/system/controller/PlaybackSoundService.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/system/controller/PlaybackSoundService.java
@@ -10,12 +10,10 @@ import android.util.Log;
 
 import com.askey.dvr.cdr7010.setting.PlaybackSound;
 import com.askey.dvr.cdr7010.setting.application.SettingApplication;
-import com.askey.dvr.cdr7010.setting.module.system.ui.PlaybackSoundSetting;
 
 public class PlaybackSoundService extends Service {
     private AudioManager mAudioManager = (AudioManager) SettingApplication.getContext().getSystemService(this.AUDIO_SERVICE);
     private int currentVolume;
-    private int tempVolume = 3;
 
     @Nullable
     @Override
@@ -31,41 +29,16 @@ public class PlaybackSoundService extends Service {
 
         @Override
         public int getPlaybackSoundVolume() throws RemoteException {
-            int playbackSoundVolume = 0;
             currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
             Log.i("PlaybackSoundService", "==currentVolume==" + currentVolume);
-            if (currentVolume == 0) {
-                playbackSoundVolume = 0;
-            } else if (0 < currentVolume && currentVolume < 4) {
-                playbackSoundVolume = 1;
-            } else if (3 < currentVolume && currentVolume < 7) {
-                playbackSoundVolume = 2;
-            } else if (6 < currentVolume && currentVolume < 10) {
-                playbackSoundVolume = 3;
-            } else if (9 < currentVolume && currentVolume < 13) {
-                playbackSoundVolume = 4;
-            } else if (12 < currentVolume && currentVolume < 16) {
-                playbackSoundVolume = 5;
-            }
-            Log.i("PlaybackSoundService", "==playbackSoundVolume==" + playbackSoundVolume);
-            return playbackSoundVolume;
+            return currentVolume;
         }
 
         @Override
         public boolean setPlaybackSoundVolume(int volume) throws RemoteException {
-            if(volume == 0){
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
-            }else if(volume == 1){
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 3, 0);
-            }else if(volume == 2){
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 6, 0);
-            }else if(volume == 3){
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 9, 0);
-            }else if(volume == 4){
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 12, 0);
-            }else if(volume == 5){
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 15, 0);
-            }else {
+            if ((volume >= 0) && (volume <= 5)) {
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+            } else {
                 return false;
             }
             return true;
--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/system/ui/PlaybackSoundSetting.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/system/ui/PlaybackSoundSetting.java
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 
 import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.base.BaseActivity;
-import com.askey.dvr.cdr7010.setting.util.Const;
 import com.askey.platform.AskeySettings;
 
 /**
@@ -55,22 +54,22 @@ public class PlaybackSoundSetting extends BaseActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         switch (keyCode) {
             case KeyEvent.KEYCODE_DPAD_DOWN:
-                currentVolumeMusic = currentVolumeMusic - 3;
+                currentVolumeMusic = currentVolumeMusic - 1;
                 if (currentVolumeMusic < 0) {
                     currentVolumeMusic = 0;
                 }
                 mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolumeMusic, 0);
-                Settings.Global.putInt(contentResolver, AskeySettings.Global.SYSSET_PLAYBACK_VOL, volumeType(currentVolumeMusic));
+                Settings.Global.putInt(contentResolver, AskeySettings.Global.SYSSET_PLAYBACK_VOL, currentVolumeMusic);
                 refreshView();
                 testPlaybackSound();
                 break;
             case KeyEvent.KEYCODE_DPAD_UP:
-                currentVolumeMusic = currentVolumeMusic + 3;
-                if (currentVolumeMusic > 15) {
-                    currentVolumeMusic = 15;
+                currentVolumeMusic = currentVolumeMusic + 1;
+                if (currentVolumeMusic > 5) {
+                    currentVolumeMusic = 5;
                 }
                 mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolumeMusic, 0);
-                Settings.Global.putInt(contentResolver, AskeySettings.Global.SYSSET_PLAYBACK_VOL, volumeType(currentVolumeMusic));
+                Settings.Global.putInt(contentResolver, AskeySettings.Global.SYSSET_PLAYBACK_VOL, currentVolumeMusic);
                 refreshView();
                 testPlaybackSound();
                 break;
@@ -80,42 +79,42 @@ public class PlaybackSoundSetting extends BaseActivity {
 
     private void refreshView() {
         getPlaybackSoundInfo();
-        if (volumeType(currentVolumeMusic) == 0) {
+        if (currentVolumeMusic == 0) {
             playBackSound0.setVisibility(View.VISIBLE);
             playBackSound1.setVisibility(View.INVISIBLE);
             playBackSound2.setVisibility(View.INVISIBLE);
             playBackSound3.setVisibility(View.INVISIBLE);
             playBackSound4.setVisibility(View.INVISIBLE);
             playBackSound5.setVisibility(View.INVISIBLE);
-        } else if (volumeType(currentVolumeMusic) == 1) {
+        } else if (currentVolumeMusic == 1) {
             playBackSound0.setVisibility(View.INVISIBLE);
             playBackSound1.setVisibility(View.VISIBLE);
             playBackSound2.setVisibility(View.INVISIBLE);
             playBackSound3.setVisibility(View.INVISIBLE);
             playBackSound4.setVisibility(View.INVISIBLE);
             playBackSound5.setVisibility(View.INVISIBLE);
-        } else if (volumeType(currentVolumeMusic) == 2) {
+        } else if (currentVolumeMusic == 2) {
             playBackSound0.setVisibility(View.INVISIBLE);
             playBackSound1.setVisibility(View.VISIBLE);
             playBackSound2.setVisibility(View.VISIBLE);
             playBackSound3.setVisibility(View.INVISIBLE);
             playBackSound4.setVisibility(View.INVISIBLE);
             playBackSound5.setVisibility(View.INVISIBLE);
-        } else if (volumeType(currentVolumeMusic) == 3) {
+        } else if (currentVolumeMusic == 3) {
             playBackSound0.setVisibility(View.INVISIBLE);
             playBackSound1.setVisibility(View.VISIBLE);
             playBackSound2.setVisibility(View.VISIBLE);
             playBackSound3.setVisibility(View.VISIBLE);
             playBackSound4.setVisibility(View.INVISIBLE);
             playBackSound5.setVisibility(View.INVISIBLE);
-        } else if (volumeType(currentVolumeMusic) == 4) {
+        } else if (currentVolumeMusic == 4) {
             playBackSound0.setVisibility(View.INVISIBLE);
             playBackSound1.setVisibility(View.VISIBLE);
             playBackSound2.setVisibility(View.VISIBLE);
             playBackSound3.setVisibility(View.VISIBLE);
             playBackSound4.setVisibility(View.VISIBLE);
             playBackSound5.setVisibility(View.INVISIBLE);
-        } else if (volumeType(currentVolumeMusic) == 5) {
+        } else if (currentVolumeMusic == 5) {
             playBackSound0.setVisibility(View.INVISIBLE);
             playBackSound1.setVisibility(View.VISIBLE);
             playBackSound2.setVisibility(View.VISIBLE);
@@ -149,24 +148,5 @@ public class PlaybackSoundSetting extends BaseActivity {
         //当前音量
         currentVolumeMusic = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
         Log.i("PlaybackSoundSetting", "==currentVolumeMusic=" + currentVolumeMusic);
-
-    }
-
-    private int volumeType(int currentVolumeMusic) {
-        int volumeType = 0;
-        if (currentVolumeMusic == 0) {
-            volumeType = 0;
-        } else if (currentVolumeMusic > 0 && currentVolumeMusic < 4) {
-            volumeType = 1;
-        } else if (currentVolumeMusic > 3 && currentVolumeMusic < 7) {
-            volumeType = 2;
-        } else if (currentVolumeMusic > 6 && currentVolumeMusic < 10) {
-            volumeType = 3;
-        } else if (currentVolumeMusic > 9 && currentVolumeMusic < 13) {
-            volumeType = 4;
-        } else if (currentVolumeMusic > 12 && currentVolumeMusic < 16) {
-            volumeType = 5;
-        }
-        return volumeType;
     }
 }
