40493016e9350a248d923c1e7794e6dceb17d741
Beck Chen(陈立_Askey_CT) <beck_chen@askey.com.tw>
2018-06-13 09:57:55 +0800

    删除升级广播所带参数

--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/emergency/ui/EmergencySwitchActivity.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/emergency/ui/EmergencySwitchActivity.java
@@ -7,6 +7,7 @@ import android.widget.AdapterView;
 
 import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.base.SecondBaseActivity;
+import com.askey.dvr.cdr7010.setting.util.Const;
 
 public class EmergencySwitchActivity extends SecondBaseActivity implements AdapterView.OnItemClickListener{
 
@@ -24,11 +25,18 @@ public class EmergencySwitchActivity extends SecondBaseActivity implements Adapt
         menuInfo = getResources().getStringArray(R.array.all_switch_item);
         switch_tag = getResources().getString(R.string.emergency_automatic_setting);
         initView(switch_tag, R.drawable.icon_submenu_audio_guide,menuInfo, R.layout.second_menu_layout);
+        jvcRelativeLayout.setMarquee_visible(true);
+        jvcRelativeLayout.setMarquee_text("OFFに設定すると、衝撃を検知したときでもコールセンターに自動通報しません");
         list_view.setOnItemClickListener(this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        String clickItem = currentData.get(position).get("menu_item").toString();
+        if (clickItem.equals(Const.ON)) {
 
+        } else if (clickItem.equals(Const.OFF)) {
+
+        }
     }
 }
