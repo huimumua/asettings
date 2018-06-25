52ff3a7ada9a7588f0ab19c3282f8855982b587f
Lisa Peng (彭玉芳_Askey_CT) <lisa_peng@askey.com.tw>
2018-06-25 10:48:21 +0800

    PUCDR-404[Settings] Emergency Contact Person

--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/emergency/ui/EmergencyContactPerson.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/emergency/ui/EmergencyContactPerson.java
@@ -1,10 +1,15 @@
 package com.askey.dvr.cdr7010.setting.module.emergency.ui;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.base.BaseActivity;
+import com.askey.dvr.cdr7010.setting.widget.JVCRelativeLayout;
+import com.askey.platform.AskeySettings;
 
 /**
  * 项目名称：settings
@@ -17,13 +22,22 @@ import com.askey.dvr.cdr7010.setting.base.BaseActivity;
  */
 public class EmergencyContactPerson extends BaseActivity {
     private static final String TAG = "EmergencyContactPerson";
+    private TextView name, number;
+    private ContentResolver contentResolver;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication_emergency_contact_person);
-
+        contentResolver = getContentResolver();
+        setRightView(false, true, false);
+        setBottomView(R.drawable.tag_menu_sub_cancel);
+        setRightView(false, 0, true, R.drawable.tag_menu_sub_ok, false, 0);
+        name = (TextView) findViewById(R.id.content_info);
+        String emergencyName = Settings.Global.getString(contentResolver, AskeySettings.Global.COMM_EMERGENCY_NAME);
+        name.setText(emergencyName);
+        number = (TextView) findViewById(R.id.content_number);
+        String emergencyNumber = Settings.Global.getString(contentResolver, AskeySettings.Global.COMM_EMERGENCY_NUMBER);
+        number.setText(emergencyNumber);
     }
-
-
 }
