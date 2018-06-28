623cebca7e9187d2f9130ee08da950fdd1052d52
Lisa Peng (彭玉芳_Askey_CT) <lisa_peng@askey.com.tw>
2018-06-28 14:51:50 +0800

    PUCDR-1196[Settings] お問合せ先（Inquiries）

--- app/src/main/java/com/askey/dvr/cdr7010/setting/module/service/ui/HelpActivity.java
+++ app/src/main/java/com/askey/dvr/cdr7010/setting/module/service/ui/HelpActivity.java
@@ -1,10 +1,15 @@
 package com.askey.dvr.cdr7010.setting.module.service.ui;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.askey.dvr.cdr7010.setting.R;
 import com.askey.dvr.cdr7010.setting.base.BaseActivity;
+import com.askey.dvr.cdr7010.setting.util.Logg;
+import com.askey.platform.AskeySettings;
 
 /**
  * 项目名称：settings
@@ -15,14 +20,29 @@ import com.askey.dvr.cdr7010.setting.base.BaseActivity;
  * 修改时间：2018/4/28 13:50
  * 修改备注：
  */
-public class HelpActivity  extends BaseActivity {
+public class HelpActivity extends BaseActivity {
+    private TextView name, number;
+    private ContentResolver contentResolver;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        setRightView(false,true,false);
+        contentResolver = getContentResolver();
+        setRightView(false, true, false);
         setBottomView(R.drawable.tag_menu_sub_cancel);
-        setRightView(false,0,true,R.drawable.tag_menu_sub_ok,false,0);
+        setRightView(false, 0, true, R.drawable.tag_menu_sub_ok, false, 0);
+        name = (TextView) findViewById(R.id.tv_content_info);
+        String contactName = Settings.Global.getString(contentResolver, AskeySettings.Global.COMM_INQUIRY_NAME);
+        Logg.i("HelpActivity", "==contactName=" + contactName);
+        if (null != contactName) {
+            name.setText(contactName);
+        }
+        number = (TextView) findViewById(R.id.tv_content_number);
+        String contactNumber = Settings.Global.getString(contentResolver, AskeySettings.Global.COMM_INQUIRY_NO);
+        Logg.i("HelpActivity", "==contactNumber=" + contactNumber);
+        if (null != contactNumber) {
+            number.setText(contactNumber);
+        }
     }
-
 }
